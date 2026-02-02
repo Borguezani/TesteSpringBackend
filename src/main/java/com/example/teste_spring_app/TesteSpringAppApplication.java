584f2fff76a7f11be39ff1f5dd8d679edfc406e7
Daniel Borguezani <daniel_borguezani@hotmail.com>
2026-02-02 16:29:51 -0300

    Init spring boot project

---
+++ src/main/java/com/example/teste_spring_app/TesteSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.teste_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSpringAppApplication.class, args);
+	}
+
+}
