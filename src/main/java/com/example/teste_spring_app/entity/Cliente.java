d604382ea8a65866ffd7460ca4446da69cbcecc1
Daniel Borguezani <daniel_borguezani@hotmail.com>
2026-02-02 17:33:09 -0300

    feat: Adciona as entidades para Usuario, Cliente e RefreshToken

---
+++ src/main/java/com/example/teste_spring_app/entity/Cliente.java
@@ -0,0 +1,38 @@
+package com.example.teste_spring_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String logradouro;
+
+    @Column(nullable = false)
+    private String bairro;
+
+    @Column(nullable = false)
+    private String cidade;
+
+    @Column(nullable = false)
+    private String estado;
+
+    @Column(nullable = false)
+    private String cep;
+}
